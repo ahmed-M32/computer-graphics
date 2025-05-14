da39d26782cca93afe99469dd0c4eb9a02c83cf3
ahmed-M32 <am.3443rrrr@gmail.com>
2025-05-14 17:27:30 +0300

    Refactor code structure for improved readability and maintainability

--- bezier-curve/main.cpp
+++ bezier-curve/main.cpp
@@ -4,7 +4,7 @@
 POINT currentPoints[4];
 int clickCount = 0;
 
-void DrawBresenhamLine(HWND hwnd, POINT pts[2],COLORREF c1);
+void DrawBezierCurve(HWND hwnd, POINT pts[4],COLORREF c1,COLORREF c2, COLORREF c3, COLORREF c4);
 void setPixel(HWND hwnd ,int x,int y,COLORREF c);
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
@@ -17,9 +17,12 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
         std::cout << "Click " << clickCount + 1 << ": (" << x << ", " << y << ")" << std::endl;
         clickCount++;
 
-        if (clickCount == 2) {
+        if (clickCount == 4) {
             COLORREF c1 = RGB(255, 0, 0);
-            DrawBresenhamLine(hwnd, currentPoints ,c1);
+            COLORREF c2 = RGB(0, 255, 0);
+            COLORREF c3 = RGB(0, 0, 255);
+            COLORREF c4 = RGB(100, 100, 100);
+            DrawBezierCurve(hwnd, currentPoints ,c1,c2,c3,c4);
             clickCount = 0;
         }
         return 0;
@@ -33,46 +36,35 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
 
 
 
+//the eqution  = u*u*u*p1 + 3*u*u*t*p1 + 3*u*t*t*p3 + t*t*t*p4;
 
-
-void DrawBresenhamLine(HWND hwnd, POINT pts[2], COLORREF c1) {
+void DrawBezierCurve(HWND hwnd, POINT pts[4],COLORREF c1,COLORREF c2 , COLORREF c3, COLORREF c4) {
     HDC hdc = GetDC(hwnd);
+    for (double i = 0; i < 1; i+=0.001) {
+        double u = 1-i;
 
-    int x0 = pts[0].x;
-    int y0 = pts[0].y;
-    int x1 = pts[1].x;
-    int y1 = pts[1].y;
+        double x= u*u*u*pts[0].x + 3*u*u*i*pts[1].x + 3*u*i*i*pts[2].x + i*i*i*pts[3].x;
+        double y = u*u*u*pts[0].y + 3*u*u*i*pts[1].y + 3*u*i*i*pts[2].y + i*i*i*pts[3].y;
 
-    int dx = abs(x1 - x0);
-    int dy = abs(y1 - y0);
-    int sx = (x0 < x1) ? 1 : -1;
-    int sy = (y0 < y1) ? 1 : -1;
-    int err = dx - dy;
-    SetPixel(hdc, x0, y0, c1);
+        double r =  u*u*u* GetRValue(c1) + 3*u*u*i*GetRValue(c2) + 3*u*i*i*GetRValue(c3) +i*i*i*GetRValue(c4);
+        double g =  u*u*u* GetGValue(c1) + 3*u*u*i*GetGValue(c2) + 3*u*i*i*GetGValue(c3) +i*i*i*GetGValue(c4);
+        double b =  u*u*u* GetBValue(c1) + 3*u*u*i*GetBValue(c2) + 3*u*i*i*GetBValue(c3) +i*i*i*GetBValue(c4);
 
-    while (true) {
-        if (x0 == x1 && y0 == y1) break;
 
-        int e2 = 2 * err;
-        if (e2 > -dy) {
-            err -= dy;
-            x0 += sx;
-        }
-        if (e2 < dx) {
-            err += dx;
-            y0 += sy;
-        }
+        SetPixel(hdc,x,y,RGB(r,g,b));
     }
 
     ReleaseDC(hwnd, hdc);
 }
 
 
+/*void setPixel(HWND hwnd, int x, int y, COLORREF c) {
 
+}*/
 
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow) {
-    const char CLASS_NAME[] = "BresenhamLineWindow";
+    const char CLASS_NAME[] = "BezierClickWindow";
 
     WNDCLASS wc = {};
     wc.lpfnWndProc = WindowProc;
@@ -84,7 +76,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow) {
     HWND hwnd = CreateWindowEx(
         0,
         CLASS_NAME,
-        "Click twice to Draw a line",
+        "Click to Draw Bézier Curves",
         WS_OVERLAPPEDWINDOW,
         CW_USEDEFAULT, CW_USEDEFAULT, 800, 600,
         NULL, NULL, hInstance, NULL
